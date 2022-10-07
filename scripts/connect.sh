dfe2764723c6b895f3947b04de314363f36129fb
Chris Wolf <Christopher.Wolf@ibm.com>
2022-10-07 14:01:05 -0400

    Gets all data on the current open issues

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-# gh api /octocat --method GET
-gh api --header 'Accept: application/vnd.github.v3+json' --method GET /repos/Christopher-Wolf/Zenhub-Integration-POC/issues | jq .[].title
+gh api --header 'Accept: application/vnd.github.v3+json' --method GET /repos/Christopher-Wolf/Zenhub-Integration-POC/issues
