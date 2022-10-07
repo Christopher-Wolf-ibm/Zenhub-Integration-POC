6bc0e15de2c5eddfb604086c75ba66308b5ce2c8
Chris Wolf <Christopher.Wolf@ibm.com>
2022-10-07 13:23:16 -0400

    Get just the title of each issue in the project

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-gh api /octocat --method GET
+# gh api /octocat --method GET
+gh api --header 'Accept: application/vnd.github.v3+json' --method GET /repos/Christopher-Wolf/Zenhub-Integration-POC/issues | jq .[].title
