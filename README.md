e6df6b8fd82c5683ffab84943a3853514e7c86a8
Chris Wolf <Christopher.Wolf@ibm.com>
2022-11-04 10:35:01 -0400

    Add integration for ZENHUB_KEY and GITHUB_REPO_ID

--- README.md
+++ README.md
@@ -3,6 +3,26 @@
 This is a playground to determine how to use the ZenHub API in order to automate adding a sprint
 to a new issue when that sprint begins.
 
+## Setup
+
+You will need a few environment variables to make this application work.
+
+### ZenHub API Key
+You will need an API key to authorize access to ZenHub.  It is expected to be stored as the `ZENHUB_KEY` environment variable.  Documentation to acquire a ZenHub API key (for IBM users only)
+can be found [here](https://zenhub.ibm.com/settings/tokens).
+
+## GitHub Repository ID
+You will need the repository ID for the GitHub repo you are trying to work with.  It is expected
+to be stored as `GITHUB_REPO_ID`.  You can acquire the repo ID by running the below command
+
+```
+curl https://api.github.com/repos/<username>/<reponame>
+```
+
+It will be stored as the `id` field in the returned JSON.
+NOTE: You may need to include a GitHub API key in your curl header if you are working with
+a private repository.
+
 ## Usage
 
 `go run cmd/cmd.go`
--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,37 +3,69 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/machinebox/graphql"
 )
 
 func main() {
-	ENDPOINT := "https://api.spacex.land/graphql/"
+	// ENDPOINT := "https://api.spacex.land/graphql/"
+	ENDPOINT := "https://api.zenhub.com/public/graphql"
+
+	GITHUB_REPO_ID := os.Getenv("GITHUB_REPO_ID")
+	// ZENHUB_KEY := os.Getenv("ZENHUB_KEY")
 
 	graphqlClient := graphql.NewClient(ENDPOINT)
 	graphqlRequest := graphql.NewRequest(`
-	{
-		launchesPast(limit: 10) {
-		  mission_name
-		  launch_date_local
-		  launch_site {
-			site_name_long
+	query getIssueInfo($repositoryGhId: Int!, $issueNumber: Int!, $workspaceId: ID!) {
+		issueByInfo(repositoryGhId: $repositoryGhId, issueNumber: $issueNumber) {
+		  id
+		  repository {
+			id
+			ghId
 		  }
-		  links {
-			article_link
-			video_link
+		  number
+		  title
+		  body
+		  state
+		  pipelineIssue(workspaceId: $workspaceId) {
+			priority {
+			  id
+			  name
+			  color
+			}
+			pipeline {
+			  id
+			  name
+			}
 		  }
-		  rocket {
-			rocket_name
+		  estimate {
+			value
+		  }
+		  sprints(first: 10) {
+			nodes {
+			  id
+			  name
+			}
+		  }
+		  labels(first: 10) {
+			nodes {
+			  id
+			  name
+			  color
+			}
 		  }
 		}
 	  }
 	`)
+	graphqlRequest.Var("issueNumber", 1)
+	graphqlRequest.Var("repositoryGhId", GITHUB_REPO_ID)
 
 	var graphqlResponse interface{}
 	err := graphqlClient.Run(context.Background(), graphqlRequest, &graphqlResponse)
 	if err != nil {
 		fmt.Println("could not complete request")
+		fmt.Printf("%v\n", err)
 	} else {
 		fmt.Println(graphqlResponse)
 	}
